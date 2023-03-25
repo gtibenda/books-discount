1a232257ec5c7c6c654de7192e40319b4b0ff9a1
Gentianus Tibenda <gentianus.tibenda@scailex.group>
2023-03-25 14:40:28 +0300

    create bookService to prepare transaction and calculate discount

---
+++ src/Shop/BookService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Shop;
+
+
+class BookService
+{
+    public function __construct(private readonly DiscountMachineInterface $discountMachine)
+    {}
+
+    private function prepareTransaction(array $items): BookTransaction
+    {
+        return new BookTransaction($items);
+    }
+
+    public function calculateDiscount($items): string
+    {
+        return $this->discountMachine->execute($this->prepareTransaction($items));
+    }
+}

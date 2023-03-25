caaa3b20068c93d28d35842fbd1c830ec43ab8e6
Gentianus Tibenda <gentianus.tibenda@scailex.group>
2023-03-25 14:35:42 +0300

    create command console skeleton that will act as interface

---
+++ src/Command/PurchaseBookCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Command;
+
+
+
+use App\Shop\BookService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:calculate-discount',
+    description: 'Calculates and outputs Discount.',
+    aliases: ['app:calculate-discount'],
+    hidden: false
+)]
+class PurchaseBookCommand extends Command
+{
+
+    /**
+     * @param BookService $bookService
+     */
+    public function __construct(protected BookService $bookService)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->addArgument('quantity1', InputArgument::REQUIRED, "Number of books.");
+        $this->addArgument('quantity2', InputArgument::OPTIONAL, "Number of books.");
+        $this->addArgument('quantity3', InputArgument::OPTIONAL, "Number of books.");
+        $this->addArgument('quantity4', InputArgument::OPTIONAL, "Number of books.");
+        $this->addArgument('quantity5', InputArgument::OPTIONAL, "Number of books.");
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $items = [
+            $input->getArgument('quantity1'),
+            $input->getArgument('quantity2'),
+            $input->getArgument('quantity3'),
+            $input->getArgument('quantity4'),
+            $input->getArgument('quantity5')
+        ];
+        ;
+        $output->writeln($this->bookService->calculateDiscount($items));
+
+        return Command::SUCCESS;
+    }
+}
